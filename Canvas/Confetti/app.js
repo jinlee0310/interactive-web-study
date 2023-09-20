77bb6a558b10c95f8b7f8206715ad031b856985d
jinlee0310 <onenmore.jin@gmail.com>
2023-09-20 22:50:43 +0900

    feat: add Particle class

--- Canvas/Confetti/app.js
+++ Canvas/Confetti/app.js
@@ -1,4 +1,4 @@
-import Particle from "./js/Particle"
+import Particle from "./js/Particle.js"
 
 const canvas=document.querySelector('canvas')
 const ctx=canvas.getContext('2d')
@@ -9,6 +9,8 @@ let canvasWidth=innerWidth
 let canvasHeight=innerHeight
 
 const interval=1000/60
+
+const particles=[]
 
 function init(){
     canvasWidth=innerWidth
@@ -21,18 +23,24 @@ function init(){
     canvas.height=canvasHeight * dpr
 
     ctx.scale(dpr,dpr)
+
+    confetti({
+        x:canvasWidth/2,
+        y:canvasHeight/2,
+        count:10,
+        deg:-50
+    })
+}
+
+function confetti({x,y,count,deg}){
+    for(let i=0;i<count;i++){
+        particles.push(new Particle(x,y,deg))
+    }
 }
 
 function render(){
     let now, delta;
     let then=Date.now()
-
-    const x=innerWidth/2
-    let y=innerHeight/2
-    let widthAlpha=0
-    const width=50
-    const height=50
-    let deg=0.1
 
     const frame=()=>{
         requestAnimationFrame(frame)
@@ -41,27 +49,27 @@ function render(){
         if(delta<interval) return
         ctx.clearRect(0,0,canvasWidth,canvasHeight)
 
-        widthAlpha+=0.1
-        deg+=0.1    
-        y+=1
+        for(let i=particles.length-1;i>=0;i--){
+            particles[i].update()
+            particles[i].draw(ctx)
+        }
 
-        ctx.translate(x+width,y+height)
-        ctx.rotate(deg)
-        ctx.translate(-x-width,-y-height)
-
-        ctx.fillStyle='red'
-        ctx.fillRect(x,y,width*Math.cos(widthAlpha),height*Math.sin(widthAlpha))
-
-        ctx.resetTransform()
 
         then=now-(delta%interval)
     }
     requestAnimationFrame(frame)
 }
 
+window.addEventListener('click',()=>{
+    confetti({
+        x:0,
+        y:canvasHeight/2,
+        count:10,
+        deg:-50
+    })
+})
+window.addEventListener('resize',init)
 window.addEventListener('load',()=>{
     init()
     render()
 })
-
-window.addEventListener('resize',init)
