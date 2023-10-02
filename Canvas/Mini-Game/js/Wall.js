fdb46b1cb1dbb10a1598d3874b12b692ae5ca9c2
jinlee0310 <onenmore.jin@gmail.com>
2023-10-02 22:47:33 +0900

    feat: Wall.js에 주석 추가

--- Canvas/Mini-Game/js/Wall.js
+++ Canvas/Mini-Game/js/Wall.js
@@ -1,4 +1,5 @@
 import App from "./App.js"
+import BoundingBox from "./BoundingBox.js"
 import { randomNumBetween } from "./util.js"
 
 export default class Wall{
@@ -15,17 +16,26 @@ export default class Wall{
                 this.sx=this.img.width*(0/30)
                 break
         }
+        // 벽의 넓이
         this.width=App.height*this.sizeX
+        // 벽의 높이
         this.height=App.height
+        // 벽의 위아래 간격
         this.gapY=randomNumBetween(App.height*0.2,App.height*0.35)
+        // 벽의 시작점의 x좌표(화면 가장 오른쪽)
         this.x=App.width
-        // -this.height
-        // App.height-this.gapY-this.height
+        // 윗벽 y좌표
         this.y1=-this.height+randomNumBetween(30,App.height-this.gapY-30)
+        // 아랫벽 y좌표
         this.y2=this.y1+this.height+this.gapY
 
+        // 다음 벽을 만드는 데 판단하는 변수
         this.generatedNext=false
         this.gapNextX=App.width*randomNumBetween(0.6,0.75)
+
+        // draw bounding box
+        this.boundingBox1=new BoundingBox(this.x,this.y1,this.width,this.height)
+        this.boundingBox2=new BoundingBox(this.x,this.y2,this.width,this.height)
     }
 
     get isOutside(){
@@ -40,6 +50,7 @@ export default class Wall{
 
     update(){
         this.x+=-6
+        this.boundingBox1.x=this.boundingBox2.x=this.x
 
     }
 
@@ -54,5 +65,8 @@ export default class Wall{
             this.sx,0,this.img.width*this.sizeX,this.img.height,
             this.x,this.y2,this.width,this.height
         )
+
+        this.boundingBox1.draw()
+        this.boundingBox2.draw()
     }
 }
